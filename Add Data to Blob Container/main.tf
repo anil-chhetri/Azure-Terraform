c8cd2fd62f557a576ac0fc9c95daa4efcaa9c719
anil-chhetri <59021082+anil-chhetri@users.noreply.github.com>
2022-05-29 16:31:23 +0545

    changed

--- Add Data to Blob Container/main.tf
+++ Add Data to Blob Container/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = "c2eed73e-337b-4549-a859-4e290835acf3"
-    client_id = "a7f52333-f167-4199-9ffc-4f81d6088d92"
-    tenant_id = "7680316a-5e9e-4cc5-a52e-9ee89ee8c404"
-    client_secret = "khB8Q~cYzvMPyuPxpCtKCGxDXj23_4S8yN2Bvabk"
+    subscription_id = ""
+    client_id = " "
+    tenant_id = " "
+    client_secret = " k"
   features {  }
 }
 
